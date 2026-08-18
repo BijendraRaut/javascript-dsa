6b3a941411b5d83d7efc19636ce8cedcfc02464e
BijendraRaut <bijendraghvm@gmail.com>
2026-08-18 22:02:58 +0530

    convert-string-uppercase-covering-edge-cases

--- strings/06-convert-string-to-uppercase-manually.js
+++ strings/06-convert-string-to-uppercase-manually.js
@@ -7,9 +7,26 @@ function convertUppercase(str) {
   let result = "";
   for (let i = 0; i < str.length; i++) {
     let code = str[i].charCodeAt(0);
-    result += String.fromCharCode(code - 32);
+    if (code >= 97 && code <= 122) {
+      result += String.fromCharCode(code - 32);
+    } else {
+      result += str[i];
+    }
   }
   return result;
 }
 
 console.log(convertUppercase("hello"));
+// HELLO
+
+console.log(convertUppercase("mane"));
+// MANE
+
+console.log(convertUppercase("hello123"));
+// HELLO123
+
+console.log(convertUppercase("Hello World"));
+// HELLO WORLD
+
+console.log(convertUppercase("JavaScript is Awesome!"));
+// JAVASCRIPT IS AWESOME!
