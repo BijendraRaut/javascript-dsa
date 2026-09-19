d76c686a4dfc35d4c7ca1458e20a1ac90a5c0737
BijendraRaut <bijendraghvm@gmail.com>
2026-09-19 23:00:24 +0530

    feat(arrays): solve problems 123

---
+++ arrays/123-move-zeros.js
@@ -0,0 +1,14 @@
+function moveZerosToEnd(arr) {
+  let j = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      j++;
+    }
+  }
+
+  return arr;
+}
+
+console.log(moveZerosToEnd([0, 1, 0, 3, 12]));
